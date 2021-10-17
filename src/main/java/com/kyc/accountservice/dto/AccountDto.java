5f6fdcb88f0a8039fd48d8650a6faf16e3aba9d3
Ali Majeed <alimajeed@live.com>
2021-10-17 16:38:46 +0500

    Dtos updated

--- src/main/java/com/kyc/accountservice/dto/AccountDto.java
+++ src/main/java/com/kyc/accountservice/dto/AccountDto.java
@@ -1,15 +1,23 @@
 package com.kyc.accountservice.dto;
 
+import com.kyc.accountservice.validationgroups.CreateRequest;
+import com.kyc.accountservice.validationgroups.UpdateRequest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccountDto {
+    @Null(groups = {CreateRequest.class, UpdateRequest.class})
     String accountNumber;
+
+    @Null(groups = CreateRequest.class)
     double accountBalance;
 }
--- src/main/java/com/kyc/accountservice/dto/UserDto.java
+++ src/main/java/com/kyc/accountservice/dto/UserDto.java
@@ -46,6 +46,7 @@ public class UserDto {
 
     CompanyDto company;
 
+    @Valid
     AccountDto account;
 
     @NotNull(groups = CreateRequest.class)
